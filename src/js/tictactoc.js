48dff521f7e5358c46e17e048a2dbfc701bbf524
stevenzm22 <stevenjosez03@gmail.com>
2025-02-12 15:51:53 -0600

    proyecto

--- src/js/tictactoc.js
+++ src/js/tictactoc.js
@@ -1,13 +1,30 @@
-let cuadro= document.getElementsByClassName('cuadro')
+const cuadro = document.getElementsByClassName('cuadro')
+const contenedorDeResultados=document.getElementById('"contenedorDeResultados')
+const reiniciar= document.getElementById('reiniciar')
 
-let lista = []
-
-
-cuadro.addEventListener('click',function () {
-for
+for (let index = 0; index < cuadro.length; index++) {
 
     
-})
+ // console.log(cuadro);
+ 
+    cuadro[index].addEventListener("click", function () {
+
+        cuadro[index].textContent="x"
+        
+      console.log("clic", (index + ""));
+
+    function rival() {
+        let turno= cuadro[index].textContent
+        turno=Math.floor(Math.random() * cuadro[index])
+        
+        turno.textContent="0"
+        console.log(turno);
+        
+    }
+      rival()
+    })
+
+}
 
 
 
