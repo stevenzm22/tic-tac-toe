71f08777d0976805c426678be4901bc0dbc19309
stevenzm22 <stevenjosez03@gmail.com>
2025-02-17 15:42:17 -0600

    ya logre corregir el error de sobre escribir

--- src/js/tictactoc.js
+++ src/js/tictactoc.js
@@ -1,54 +1,83 @@
 const cuadro = document.getElementsByClassName('cuadro')
-const contenedorDeResultados=document.getElementById('"contenedorDeResultados')
+const contenedorDeResultados=document.getElementById('contenedorDeResultados')
 const reiniciar= document.getElementById('reiniciar')
+
+let listaVacia = [];
+let listaOcupada = [];
+let listaRestante = []
+
 
 
 for (let index = 0; index < cuadro.length; index++) {
 
- // console.log(cuadro);
- 
+    listaVacia.push(index)
+
     cuadro[index].addEventListener("click", function () {
 
-        if (cuadro[index].textContent != "") {
-            console.log("holi");
-            
+        if (cuadro[index].textContent !== "") {
+            alert("esta lleno")
         }
-
-        cuadro[index].textContent="x"
+       
+       if (cuadro[index].textContent="x") {
+            listaOcupada.push(index)
+            listaRestante = listaVacia.filter(el => !listaOcupada.includes(el));
+            rival()
+        }
         
-      console.log("clic", (index + ""));
+        
 
         //funcion para que la maquina juegue
         function rival() {
 
-            let turno= Math.floor(Math.random() * cuadro.length)
-            return turno
-          
+            let turno=listaRestante[Math.floor(Math.random() * listaRestante.length)]
+           
+            cuadro[turno].textContent= "0"
+         }
+         console.log(rival());
+         
+       // validacion de ganar 
+                const ganar=[
+                    [0,1,2],
+                    [3,4,5],
+                    [6,7,8],
+                    [0,4,8],
+                    [2,4,6],
+                    [0,3,6],
+                    [2,5,8],
+                    [1,4,7]
+                ]
+
+         for (let index = 0; index < ganar.length; index++) {
+
+          const [a,b,c] = ganar[index]
+
+          if (
+            cuadro[a].textContent === "x" &&
+            cuadro[b].textContent === "x" &&
+            cuadro[c].textContent === "x"
+        ) {
+           alert("ganaste")
+        } else if (
+            cuadro[a].textContent === "0" &&
+            cuadro[b].textContent === "0" &&
+            cuadro[c].textContent === "0"
+        ) {
+           alert("perdiste")
         }
-            let aleatorio= rival()
-             console.log(aleatorio);
 
-             
 
-             if ( cuadro[index]==="x") {
+
+         } 
                 
-              console.log("click");
-              
-              
-             } else { 
-                 cuadro[index].textContent != ""
-                cuadro[aleatorio].textContent="0"
-          
-            }
-               
-       
-        
-    })
+    }) 
 
-    
 }
 
-
+reiniciar.addEventListener("click", function () {
+    for (let index = 0; index < cuadro.length; index++) {
+        cuadro[index].textContent = ""
+    }
+});
 
 
 
